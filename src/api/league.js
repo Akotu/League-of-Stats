29501f316706e07c20f55ebec599aab14a308c25
Maverick <embrymaverick@gmail.com>
2018-09-21 11:49:06 -0700

    centering logo

---
+++ src/api/league.js
@@ -0,0 +1,2 @@
+/* eslint-disable */
+// https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=<key>
--- src/pages/player/player.js
+++ src/pages/player/player.js
@@ -1,8 +1,24 @@
 /* eslint-disable */
 import React, { Component } from "react";
+import "../../api/league";
 
 class Player extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      profileIconId: "",
+      name: "",
+      summonerLevel: "",
+      summonerID: "",
+      tier: "",
+      rank: "",
+      leaguePoints: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   render() {
     return <p>I am the player Page</p>;
